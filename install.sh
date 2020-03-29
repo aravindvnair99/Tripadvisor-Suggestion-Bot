29521ab19541555aabf5d2a13db10c4a80ba5347
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-03-30 00:28:57 +0530

    chore: initial config
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python -m pip install --upgrade pip
+pip install virtualenv
+virtualenv virtualenv
+. virtualenv/Scripts/activate
+pip install -r requirements.txt
+#python -m nltk.downloader all
