222ba1a6da89db7d915fd37fed50108687f89249
JSdheerajkumar <dheerajdheeru235@gmail.com>
2020-04-28 12:44:48 +0530

    finalcode maybe
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

---
+++ app/examplereplacer.py
@@ -0,0 +1,33 @@
+import re
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+
+
+class AntonymReplacer(object):
+    def replace(self, word):
+        ant = list()
+        for syn in wordnet.synsets(word):
+            for lemma in syn.lemmas():
+                if lemma.antonyms():
+                    ant.append(lemma.antonyms()[0].name())
+        if len(ant) >= 1:
+            return ant[0]
+        else:
+            return None
+
+    def negreplace(self, string):
+        i = 0
+        sent = word_tokenize(string)
+        len_sent = len(sent)
+        words = []
+        while i < len_sent:
+            word = sent[i]
+            if word == 'not' and i + 1 < len_sent:
+                ant = self.replace(sent[i + 1])
+                if ant:
+                    words.append(ant)
+                    i += 2
+                    continue
+            words.append(word)
+            i += 1
+        return words
